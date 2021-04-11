5c4b3acd426840f9fafcc0cfd6d90c9e7282be55
jeevesh <admin@admins-MacBook-Air.local>
2021-04-11 15:43:48 +0530

    April 11, 2021 3:43 PM

--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -3,7 +3,10 @@ import React from 'react'
 class HelloWorld extends React.Component {
 render(){
 
-return <h1> Hello {this.props.name}</h1>
+return ( 
+    <h1> Hello {this.props.name}</h1>
+        )
     }
 
 }
+export default HelloWorld
