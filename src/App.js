b093617b1850785fa0109133e96c8d4bc7a226f9
jeevesh <admin@admins-MacBook-Air.local>
2021-04-11 15:39:40 +0530

    April 11, 2021 3:39 PM

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import './App.css';
 import HelloWorld from './Components/HelloWorld';
-import Rs from './Components/Rs';
 function App() {
   return (
     <div>
      <HelloWorld name="jai" />
 
-     <p>
-     <Rs name="Jeevan" />  </p>
+     
         
     </div>
   );
