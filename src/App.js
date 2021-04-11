6c0a93fbc553bb1ef9436d4d86dac9a25ddd7989
jeevesh <admin@admins-MacBook-Air.local>
2021-04-11 15:14:58 +0530

    April 11, 2021 3:14 PM

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './Components/HelloWorld'
+import HelloWorld from './Components/HelloWorld';
 function App() {
   return (
     <div >
--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 
-function HelloWorld() {
+function HelloWorld () {
     return (
         <h1> Hellow world !! </h1>
 
